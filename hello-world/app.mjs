060c29d166b0fc8fe7d6aaf2b2a60e236fed90eb
rperandre <rperandre@padtec.com.br>
2024-07-26 11:36:13 -0300

    Preparing scrapper to be implemented.

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -1,24 +1,17 @@
+import { scrape } from './src/scrapper.mjs';
+import { publish } from './src/sns-publisher.mjs';
+
 /**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * Handles a scrape event.
  * 
+ * @param {Object} event The event to be handled.
+ * @returns A HTTP-prepared response.
  */
-
-export const lambdaHandler = async (event, context) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      })
-    };
-
-    return response;
-  };
-  
+export const lambdaHandler = async (event) => {
+    for (const record of event.Records) {
+        const body = JSON.parse(record.body);
+        const url = body.Message;
+        const message = await scrape(url);
+        await publish(message);
+    }
+};
---
+++ hello-world/src/scrapper.mjs
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+/**
+ * Scrapes the supermarket, product and price data from a NFC-e URL.
+ * 
+ * @param {String} url The NFC-e URL to be scraped.
+ * @returns An object with the scraped data.
+ */
+export const scrape = async (url) => {
+    //TODO: Implement the scraper function.
+    return {};
+};
